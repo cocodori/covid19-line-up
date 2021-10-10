589a09b01c352a69e410d3448dbf34aa50f49c4e
hoon <hoon.lee@jalsalda.com>
2021-10-10 16:04:32 +0900

    create project

---
+++ src/main/java/com/lineup/covid19lineup/Covid19LineUpApplication.java
@@ -0,0 +1,13 @@
+package com.lineup.covid19lineup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19LineUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Covid19LineUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lineup/covid19lineup/Covid19LineUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lineup.covid19lineup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19LineUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
