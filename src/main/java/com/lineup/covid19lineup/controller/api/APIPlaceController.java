489da999ac4332be656678b75cdd30a026036279
hoon <moradulee@gmail.com>
2021-10-11 15:07:47 +0900

    API Place holder 누락된 것 추가

--- src/main/java/com/lineup/covid19lineup/controller/api/APIPlaceController.java
+++ src/main/java/com/lineup/covid19lineup/controller/api/APIPlaceController.java
@@ -1,4 +1,35 @@
 package com.lineup.covid19lineup.controller.api;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api")
 public class APIPlaceController {
+    @GetMapping("/places")
+    public List<String> getPlaces() {
+        return List.of("place1", "place2");
+    }
+
+    @PostMapping("/places")
+    public Boolean createPlace() {
+        return true;
+    }
+
+    @GetMapping("/places/{placeId}")
+    public String getPlace(@PathVariable Integer placeId) {
+        return "place " + placeId;
+    }
+
+    @PutMapping("/places/{placeId}")
+    public Boolean modifyPlace(@PathVariable Integer placeId) {
+        return true;
+    }
+
+    @DeleteMapping("/places/{placeId}")
+    public Boolean removePlace(@PathVariable Integer placeId) {
+        return true;
+    }
 }
